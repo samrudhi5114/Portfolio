b620365fcf9fc772585160fe0177c80470d4d17d
samrudhi5114 <samrudhishetty05@gmail.com>
2024-11-13 10:29:00 +0530

    push portfolio

---
+++ resume.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Mail handling logic goes here
+
+    echo "Thank you, $name! Your message has been sent.";
+}
+?>
+
